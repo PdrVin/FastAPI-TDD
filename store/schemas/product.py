dcf0f1fd91443aacc13783c6e9a296097ed2c0a4
Pedro <ped.vin42cres@gmail.com>
2024-07-04 09:47:16 -0300

    Test Usecases

--- store/schemas/product.py
+++ store/schemas/product.py
@@ -1,9 +1,29 @@
-from pydantic import Field
+from typing import Optional
+from pydantic import BaseModel, Field
+
 from store.schemas.base import BaseSchemaMixin
 
 
-class ProductIn(BaseSchemaMixin):
+class ProductBase(BaseModel):
     name: str = Field(..., description="Product Name")
     quantity: int = Field(..., description="Product Quantity")
     price: float = Field(..., description="Product Price")
     status: bool = Field(..., description="Product Status")
+
+
+class ProductIn(ProductBase, BaseSchemaMixin):
+    ...
+
+
+class ProductOut(ProductIn):
+    ...
+
+
+class ProductUpdate(ProductBase):
+    quantity: Optional[int] = Field(None, description="Product Quantity")
+    price: Optional[float] = Field(None, description="Product Price")
+    status: Optional[bool] = Field(None, description="Product Status")
+
+
+class ProductUpdateOut(ProductUpdate):
+    ...
--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -1,10 +1,49 @@
+from typing import List
+from uuid import UUID
 import pytest
+
+from store.core.exceptions import NotFoundException
+from store.schemas.product import ProductOut, ProductUpdateOut
 from store.usecases.product import product_usecase
-from tests.factories import product_data
 
 
-@pytest.fixture
-async def test_usecases_should_return_success():
-    result = await product_usecase.create(body=product_data)
-    # assert isinstance(result, ProductOut)
-    assert result is None
+# CREATE -> Success
+async def test_usecases_create_should_return_success(product_in):
+    result = await product_usecase.create(body=product_in)
+
+    assert isinstance(result, ProductOut)
+    assert result.name == "Iphone 14 Pro Max"
+
+
+# GET -> Success
+async def test_usecases_get_should_return_success(product_id):
+    result = await product_usecase.get(id=product_id)
+
+    assert isinstance(result, ProductOut)
+    assert result.id == product_id
+
+
+# GET -> Not Found
+async def test_usecases_get_should_not_found():
+    with pytest.raises(NotFoundException) as err:
+        await product_usecase.get(id=UUID("1e4f214e-85f7-461a-89d0-a751a32e3bb9"))
+
+    assert (
+        err.value.message
+        == "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
+    )
+
+
+# QUERY -> Success
+async def test_usecases_query_should_return_success():
+    result = await product_usecase.query()
+
+    assert isinstance(result, List)
+
+
+# UPDATE -> Success
+async def test_usecases_update_should_return_success(product_id, product_up):
+    product_up.price = 8500
+    result = await product_usecase.update(id=product_id, body=product_up)
+
+    assert isinstance(result, ProductUpdateOut)
