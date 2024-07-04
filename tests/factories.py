1ce50c209041348351d8d12952fca51196b83076
Pedro <ped.vin42cres@gmail.com>
2024-07-04 20:12:07 -0300

    Methods & Data

--- tests/factories.py
+++ tests/factories.py
@@ -5,3 +5,12 @@ def product_data():
         "price": 8500,
         "status": True,
     }
+
+
+def products_data():
+    return [
+        {"name": "Iphone 11 Pro Max", "quantity": 20, "price": "4500", "status": True},
+        {"name": "Iphone 12 Pro Max", "quantity": 15, "price": "5500", "status": True},
+        {"name": "Iphone 13 Pro Max", "quantity": 5, "price": "6500", "status": True},
+        {"name": "Iphone 15 Pro Max", "quantity": 3, "price": "10500", "status": False},
+    ]
