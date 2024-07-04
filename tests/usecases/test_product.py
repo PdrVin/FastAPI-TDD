38df4b32db39a5bce6b41857dd313082d7eb2a86
Pedro <ped.vin42cres@gmail.com>
2024-07-04 20:12:47 -0300

    Test Usecases

--- tests/usecases/test_product.py
+++ tests/usecases/test_product.py
@@ -16,11 +16,11 @@ async def test_usecases_create_should_return_success(product_in):
 
 
 # GET -> Success
-async def test_usecases_get_should_return_success(product_id):
-    result = await product_usecase.get(id=product_id)
+async def test_usecases_get_should_return_success(product_inserted):
+    result = await product_usecase.get(id=product_inserted.id)
 
     assert isinstance(result, ProductOut)
-    assert result.id == product_id
+    assert result.name == "Iphone 14 Pro Max"
 
 
 # GET -> Not Found
@@ -35,15 +35,35 @@ async def test_usecases_get_should_not_found():
 
 
 # QUERY -> Success
+@pytest.mark.usefixtures("products_inserted")
 async def test_usecases_query_should_return_success():
     result = await product_usecase.query()
 
     assert isinstance(result, List)
+    assert len(result) > 1
 
 
 # UPDATE -> Success
-async def test_usecases_update_should_return_success(product_id, product_up):
+async def test_usecases_update_should_return_success(product_inserted, product_up):
     product_up.price = 8500
-    result = await product_usecase.update(id=product_id, body=product_up)
+    result = await product_usecase.update(id=product_inserted.id, body=product_up)
 
     assert isinstance(result, ProductUpdateOut)
+
+
+# DELETE -> Success
+async def test_usecases_delete_should_return_success(product_inserted):
+    result = await product_usecase.delete(id=product_inserted.id)
+
+    assert result is True
+
+
+# DELETE -> Not Found
+async def test_usecases_delete_should_not_found():
+    with pytest.raises(NotFoundException) as err:
+        await product_usecase.delete(id=UUID("1e4f214e-85f7-461a-89d0-a751a32e3bb9"))
+
+    assert (
+        err.value.message
+        == "Product not found with filter: 1e4f214e-85f7-461a-89d0-a751a32e3bb9"
+    )
