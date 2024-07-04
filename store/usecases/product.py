1cf188935c604df303664f7a9482964a6b8f2031
Pedro <ped.vin42cres@gmail.com>
2024-07-04 09:46:29 -0300

    Product_Usecase Methods

--- store/usecases/product.py
+++ store/usecases/product.py
@@ -1,6 +1,11 @@
+import pymongo
+from typing import List
+from uuid import UUID
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+
+from store.core.exceptions import NotFoundException
 from store.db.mongo import db_client
-from store.schemas.product import ProductIn
+from store.schemas.product import ProductIn, ProductOut, ProductUpdate, ProductUpdateOut
 
 
 class ProductUsecase:
@@ -9,8 +14,34 @@ class ProductUsecase:
         self.database: AsyncIOMotorDatabase = self.client.get_database()
         self.collection = self.database.get_collection("products")
 
-    async def create(self, body: ProductIn):
-        await self.collection.insert_one(body.model_dump())
+    # CREATE
+    async def create(self, body: ProductIn) -> ProductOut:
+        product = ProductOut(**body.model_dump())
+        await self.collection.insert_one(product.model_dump())
+
+        return product
+
+    # GET
+    async def get(self, id: UUID) -> ProductOut:
+        result = await self.collection.find_one({"id": id})
+
+        if not result:
+            raise NotFoundException(message=f"Product not found with filter: {id}")
+
+        return ProductOut(**result)
+
+    # QUERY
+    async def query(self) -> List[ProductOut]:
+        return [ProductOut(**item) async for item in self.collection.find()]
+
+    # UDPATE
+    async def update(self, id: UUID, body: ProductUpdate) -> ProductUpdateOut:
+        result = await self.collection.find_one_and_update(
+            filter={"id": id},
+            update={"$set": body.model_dump(exclude_none=True)},
+            return_document=pymongo.ReturnDocument.AFTER,
+        )
+        return ProductUpdateOut(**result)
 
 
 product_usecase = ProductUsecase()
