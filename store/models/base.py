261b62a48064f579e7467a84984b49e5dfb4f924
Pedro <ped.vin42cres@gmail.com>
2024-07-05 16:14:24 -0300

    UsecaseRefactoring

--- store/models/base.py
+++ store/models/base.py
@@ -1,9 +1,22 @@
 import uuid
+from decimal import Decimal
+from bson import Decimal128
+from typing import Any
 from datetime import datetime
-from pydantic import BaseModel, UUID4, Field
+from pydantic import BaseModel, UUID4, Field, model_serializer
 
 
 class CreateBaseModel(BaseModel):
     id: UUID4 = Field(default_factory=uuid.uuid4)
     created_at: datetime = Field(default_factory=datetime.utcnow)
     updated_at: datetime = Field(default_factory=datetime.utcnow)
+
+    @model_serializer
+    def set_model(self) -> dict[str, Any]:
+        self_dict = dict(self)
+
+        for key, value in self_dict.items():
+            if isinstance(value, Decimal):
+                self_dict[key] = Decimal128(str(value))
+
+        return self_dict
--- store/schemas/product.py
+++ store/schemas/product.py
@@ -1,14 +1,15 @@
-from datetime import datetime
-from typing import Optional
-from pydantic import UUID4, BaseModel, Field
+from decimal import Decimal
+from bson import Decimal128
+from typing import Annotated, Optional
+from pydantic import AfterValidator, BaseModel, Field
 
-from store.schemas.base import BaseSchemaMixin
+from store.schemas.base import BaseSchemaMixin, OutMixin
 
 
 class ProductBase(BaseModel):
     name: str = Field(..., description="Product Name")
     quantity: int = Field(..., description="Product Quantity")
-    price: float = Field(..., description="Product Price")
+    price: Decimal = Field(..., description="Product Price")
     status: bool = Field(..., description="Product Status")
 
 
@@ -16,17 +17,22 @@ class ProductIn(ProductBase, BaseSchemaMixin):
     ...
 
 
-class ProductOut(ProductIn):
-    id: UUID4 = Field()
-    created_at: datetime = Field()
-    updated_at: datetime = Field()
+class ProductOut(ProductIn, OutMixin):
+    ...
+
+
+def convert_decimal_128(value):
+    return Decimal128(str(value))
+
+
+Decimal_ = Annotated[Decimal, AfterValidator(convert_decimal_128)]
 
 
 class ProductUpdate(ProductBase):
     quantity: Optional[int] = Field(None, description="Product Quantity")
-    price: Optional[float] = Field(None, description="Product Price")
+    price: Optional[Decimal_] = Field(None, description="Product Price")
     status: Optional[bool] = Field(None, description="Product Status")
 
 
-class ProductUpdateOut(ProductUpdate):
+class ProductUpdateOut(ProductUpdate, OutMixin):
     ...
